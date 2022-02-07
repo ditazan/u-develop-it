210090fcb0fc5fee67891755e69822742d0fadfd
dit <ditazanelli@gmail.com>
2022-02-07 13:51:15 -0500

    database created

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+    candidates (first_name, last_name, industry_connected)
+VALUES
+    ('snooki', 'meatbal', 1),
+    ('jwoww', 'jenni', 1),
+    ('pauly', 'd', 1),
+    ('deena', 'meatball', 0),
+    ('sammi', 'sweetheart', 0),
+    ('angelina', 'dirty hamster', 0),
+    ('ronnie', 'abuser', 0),
+    ('mike', 'the situation', 1),
+    ('vinny', 'pink-eye', 0);
