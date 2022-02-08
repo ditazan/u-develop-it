856db08aa31f0a145c3877fc35da6a83423ccfbe
dit <ditazanelli@gmail.com>
2022-02-07 19:59:03 -0500

    connected database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require ('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,17 @@ const app = express();
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+  {
+    host: `localhost`,
+    user: `root`,
+    password:`PureEnrichment666!`,
+    database: `election`
+  },
+  console.log(`connected to the election database`)
+);
 
 app.get(`/`, (req, res) => {
   res.json({
